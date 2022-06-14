9565654f5773c28b72334029ec32ff4c94342fe0
PaulSchubert <paul.frederik.schubert@gmail.com>
2022-06-14 09:23:46 -0700

    Hello World commit

---
+++ COMP 250 (Summer 2022)/Paul-FrederikSchubertWeek1PracticeProg1.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Learning C++
+//
+//  Created by Paul-Frederik Schubert on 6/14/22.
+//
+
+#include <iostream>  //Name of prewritten code <file> you want to use
+                     //class will focus on reusing existing code for best practices
+                     //#include names files/libraries of existing code you want to reuse
+using namespace std; //Command to avoid typing std:: infront of cout and endl commands
+
+int main(){ //All programs start at 'main' - simple version of main, others possible
+    cout<<"Hello World"<<endl<<endl;  //cout commans to print "message" to monitor
+    return 0;  //end program
+}
