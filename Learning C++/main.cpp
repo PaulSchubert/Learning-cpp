86111c0636388fc83df6625794e4ffdba97fcb10
PaulSchubert <paul.frederik.schubert@gmail.com>
2022-06-14 09:10:35 -0700

    Initial Commit

---
+++ Learning C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learning C++
+//
+//  Created by Paul-Frederik Schubert on 6/14/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
