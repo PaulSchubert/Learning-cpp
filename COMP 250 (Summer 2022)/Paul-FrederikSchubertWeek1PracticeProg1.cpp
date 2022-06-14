07048aae41da959de421fc6e0ddfee893834c01c
PaulSchubert <paul.frederik.schubert@gmail.com>
2022-06-14 09:38:59 -0700

    Requirement edit

--- COMP 250 (Summer 2022)/Paul-FrederikSchubertWeek1PracticeProg1.cpp
+++ COMP 250 (Summer 2022)/Paul-FrederikSchubertWeek1PracticeProg1.cpp
@@ -11,6 +11,7 @@
 using namespace std; //Command to avoid typing std:: infront of cout and endl commands
 
 int main(){ //All programs start at 'main' - simple version of main, others possible
+    cout<<"Paul-Frederik Schubert - 14 June 2022"<<endl;
     cout<<"Hello World"<<endl<<endl;  //cout commans to print "message" to monitor
     return 0;  //end program
 }
