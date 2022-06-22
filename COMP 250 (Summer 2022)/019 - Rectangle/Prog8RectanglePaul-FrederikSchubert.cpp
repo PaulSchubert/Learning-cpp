0fb7ec87c6c84672209e5d853c2871643d1988b0
PaulSchubert <paul.frederik.schubert@gmail.com>
2022-06-22 13:35:35 -0700

    More class practice & building good habits

---
+++ COMP 250 (Summer 2022)/019 - Rectangle/Prog8RectanglePaul-FrederikSchubert.cpp
@@ -0,0 +1,49 @@
+//
+//  main.cpp
+//  019 - Rectangle
+//
+//  Created by Paul-Frederik Schubert on 6/22/22.
+//
+
+//classes example
+#include <iostream>
+
+using namespace std;
+
+
+class Rectangle{
+public:
+    Rectangle(){}; //variables are empty
+    Rectangle(int w, int h){
+        set_values(w, h);
+    }
+    
+    void set_values(int w, int h);
+    
+    int area(){
+        answer = width * height;
+        return answer;
+    }
+    
+private:
+    int answer, width, height;
+};
+
+void Rectangle::set_values(int x, int y){
+    width = x;
+    height = y;
+}
+
+
+int main(){
+    cout<<"Name: Paul-Frederik Schubert - Program Name: Program 8, Rectangle Class - Date: 06/22/2022"<<endl<<endl;
+    
+    Rectangle rect1(5, 6);
+    cout<<"area: "<<rect1.area()<<endl;
+    
+    Rectangle rect2;
+    rect2.set_values(3, 4);
+    cout<<"area: "<<rect2.area()<<endl;
+    
+    return 0;
+}
