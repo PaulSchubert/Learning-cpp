a6f65414a7628e0ab670e4e6249194051ceb1ced
PaulSchubert <paul.frederik.schubert@gmail.com>
2022-06-30 22:49:51 -0700

    Beginnings of my CIS 250 final project (memory card game)

---
+++ COMP 250 (Summer 2022)/000 - Final Project/Selections.h
@@ -0,0 +1,28 @@
+//
+//  Selections.h
+//  000 - Final Project
+//
+//  Created by Paul-Frederik Schubert on 6/30/22.
+//
+
+#ifndef Selections_h
+#define Selections_h
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//levelSeed, groupSeed, wordSeed selection
+class selections{
+public:
+    
+    void chooseLevel();
+    void chooseGroup();
+    void chooseSpeed();
+    
+private:
+    string chem[50], sport[50], cars[50];
+    int wordSeed, levelSeed, speedSeed;
+};
+
+#endif /* Selections_h */
---
+++ COMP 250 (Summer 2022)/000 - Final Project/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  000 - Final Project
+//
+//  Created by Paul-Frederik Schubert on 6/30/22.
+//
+
+#include <iostream>
+#include <string>
+#include "Selections.h"
+using namespace std;
+
+
+int main(){
+    cout<<"Paul-Frederik Schubert - Final Project (Memory Game)"<<endl<<endl;
+    
+    selections b1;
+    b1.chooseGroup();
+    b1.chooseLevel();
+    b1.chooseSpeed();
+    return 0;
+}
